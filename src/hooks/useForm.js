15063c1342323f37dc11e50c30a14ff14e32dd71
Mateo <mattleandri>
2023-11-03 13:23:16 -0300

    solucion numeros

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -8,34 +8,36 @@ export function useForm (form = {}) {
         setFormState(newVal)
     }
 
-    const onInputChange = ({target},validations) => {
+    const onInputChange = ({target},validations,type) => {
     
         let {name,value} = target
-        value = parseFloat(value)
+        //value = parseFloat(value) TODO: Se comento esto... ver si no genera bugs
 
         if(validations){
             switch (validations){
 
                 case  'positiveNumber':{
+                    
+                    value = parseFloat(value)
 
                     if(
                         (typeof(value)!='number')|| 
                         value < 0 || 
                         isNaN(value) 
                         //((/^0+/.test(value)) && value.toString().length>1 ) TODO: Que si el valor es ej 001 se borren los ceros
-                        ) 
-                        
-                        value = 0
-
+                        ) value=0
                 }
             }
+
         }
-
+        
         setFormState({
+            
             ...formState,
-            [name]: parseFloat(value)   //TODO: Quitar parseFloat de aqui ya que me limitaria a que solo funciona para numeros
+            [name]: value   //TODO: Quitar parseFloat de aqui ya que me limitaria a que solo funciona para numeros
         })
-
+        
+        
     }
 
     return{
